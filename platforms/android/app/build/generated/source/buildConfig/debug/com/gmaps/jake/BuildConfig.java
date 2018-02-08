bcad104f126ca788aa7db18e9e53cea4efb4f0c8
J <jack@ixi.my>
2018-02-08 10:53:04 +0800

    First

---
+++ platforms/android/app/build/generated/source/buildConfig/debug/com/gmaps/jake/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.gmaps.jake;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.gmaps.jake";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10000;
+  public static final String VERSION_NAME = "1.0.0";
+}
